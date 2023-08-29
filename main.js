5cf5313f12278de8a83367728ba43528f3b63b61
G-GERA <141760379+G-GERA@users.noreply.github.com>
2023-08-29 19:24:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+var image1=["peace.png"];
+
+var a=0;
+function peace()
+{
+if(a==1)
+{
+    a=0;
+}
+document.getElementById("img_hi").src = image1[a];
+a++;
+}
+
+var image2=["rock.png"];
+
+var b=0;
+function rock()
+{
+if(b==1)
+{
+    b=0;
+}
+document.getElementById("img_hi").src = image2[b];
+b++;
+}
+
+var image3=["one.png"];
+
+var c=0;
+function one()
+{
+if(c==1)
+{
+    c=0;
+}
+document.getElementById("img_hi").src = image3[c];
+c++;
+}
+
+var image4=["call.png"];
+
+var d=0;
+function call()
+{
+if(d==1)
+{
+    d=0;
+}
+document.getElementById("img_hi").src = image4[d];
+d++;
+}
+
+var image5=["up.png"];
+
+var e=0;
+function up()
+{
+if(e==1)
+{
+    e=0;
+}
+document.getElementById("img_hi").src = image5[e];
+e++;
+}
+
+var image6=["down.png"];
+
+var f=0;
+function down()
+{
+if(f==1)
+{
+    f=0;
+}
+document.getElementById("img_hi").src = image6[f];
+f++;
+}
+
+var image7=["hello.png"];
+
+var g=0;
+function hello()
+{
+if(g==1)
+{
+    g=0;
+}
+document.getElementById("img_hi").src = image7[g];
+g++;
+}
